eca15deef4304d7a26d7b1dc4ef26210ff1e8104
bhargav0410 <bvg8@scarletmail.rutgers.edu>
2019-01-07 23:07:38 -0500

    started ofdm mod and demod with mpi

---
+++ ofdm_mpi.h
@@ -0,0 +1,34 @@
+#ifndef OFDM_MPI_H
+#define OFDM_MPI_H
+
+#include <cstdio>
+#include <cstdlib>
+#include <mpi.h>
+#include <fftw3.h>
+#include <fftw3-mpi.h>
+
+class ofdm_mpi {
+
+public:
+    ofdm_mpi(int, int);
+    ofdm_mpi(int, int, int, int);
+    ~ofdm_mpi();
+    void create_pilot_vector(std::vector<std::vector<std::complex<float>>> &);
+    void chan_est_update_mpi(std::vector<std::vector<std::complex<float>>> &);
+    void maximal_ratio_combining_mpi(std::vector<std::vector<std::complex<float>>> &, std::vector<std::vector<std::complex<float>>> &);
+    void maximal_ratio_transmission_mpi(std::vector<std::vector<std::complex<float>>> &, std::vector<std::vector<std::complex<float>>> &);
+    void mod_one_frame_mpi(std::vector<std::vector<std::complex<float>>> &, std::vector<std::vector<std::complex<float>>> &);
+    void demod_one_frame_mpi(std::vector<std::vector<std::complex<float>>> &, std::vector<std::vector<std::complex<float>>> &);
+    int get_fft_size();
+    void set_fft_size(int);
+    int get_prefix_size();
+    void set_prefix_size(int);
+
+protected:
+    int orank, osize, fft_size, prefix_size;
+    std::vector<std::vector<std::complex<float>>> chan_est, pilot_vector;
+
+
+};
+
+#endif
